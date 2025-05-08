54e91af51c930960f2de2cfe63447bce382cae06
Praful780 <154632487+Praful780@users.noreply.github.com>
2025-05-08 23:26:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux"
-import { store } from './redux/store.js'
+import store from './redux/Store.js'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
